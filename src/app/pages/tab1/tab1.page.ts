a3cbcde72916978f51d5278b9814ee8d707591e8
190701150 <190701150@stu.istinye.edu.tr>
2023-06-05 14:13:27 +0300

    Added details page updates-5june

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,15 +1,16 @@
 import {Component, inject, Inject, OnInit} from '@angular/core';
 import {AlertController, IonicModule} from '@ionic/angular';
-import {HttpClient, HttpClientModule} from '@angular/common/http'
-import { CommonModule } from '@angular/common';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http'
+import {CommonModule} from '@angular/common';
 import {ActivatedRoute, Router} from "@angular/router";
-import { PaginatedResult } from 'src/app/models/paginatedResult';
-import { environment } from 'src/environments/environment';
-import { Car } from 'src/app/models/car';
+import {PaginatedResult} from 'src/app/models/paginatedResult';
+import {environment} from 'src/environments/environment';
+import {Car} from 'src/app/models/car';
 import {Model} from "src/app/models/model";
 import {Color} from "src/app/models/color";
 import {TurkishCurrencyPipe} from "src/app/pipes/turkish-currency.pipe";
-import { FavoritesService } from 'src/app/favorites.service';
+import {FavoritesService} from 'src/app/favorites.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
@@ -18,25 +19,28 @@ import { FavoritesService } from 'src/app/favorites.service';
   standalone: true,
   imports: [IonicModule, HttpClientModule, CommonModule, TurkishCurrencyPipe],
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
   paginatedCarResult: PaginatedResult<Car>
   paginatedModelResult: PaginatedResult<Model>
   paginatedColorResult: PaginatedResult<Color>
 
   router: Router = inject(Router)
-  constructor(private httpClient: HttpClient, private readonly alertController: AlertController, private readonly activatedRoute: ActivatedRoute, private favoritesService: FavoritesService) {}
+
+  constructor(private httpClient: HttpClient, private readonly alertController: AlertController, private readonly activatedRoute: ActivatedRoute, private favoritesService: FavoritesService, private location: Location) {
+  }
+
   ngOnInit(): void {
     this.getCars()
     this.getModels()
     this.getColors()
     this.activatedRoute.params.subscribe({
-      next:(params) => {
+      next: (params) => {
         params["carId"]
       }
     })
   }
 
-  getCars(){
+  getCars() {
     this.httpClient.get<PaginatedResult<Car>>(`${environment.apiUrl}Cars?Page=0&PageSize=1000`).subscribe({
       next: (value) => {
         this.paginatedCarResult = value;
@@ -45,7 +49,7 @@ export class Tab1Page implements OnInit{
     });
   }
 
-  getModels(){
+  getModels() {
     this.httpClient.get<PaginatedResult<Model>>(`${environment.apiUrl}Models?Page=0&PageSize=1000`).subscribe({
       next: (value) => {
         this.paginatedModelResult = value;
@@ -54,7 +58,8 @@ export class Tab1Page implements OnInit{
       }
     })
   }
-  getColors(){
+
+  getColors() {
     this.httpClient.get<PaginatedResult<Color>>(`${environment.apiUrl}Colors?Page=0&PageSize=10`).subscribe({
       next: (value) => {
         this.paginatedColorResult = value;
@@ -63,17 +68,17 @@ export class Tab1Page implements OnInit{
       }
     })
   }
+
   //getColorName(colorId: number): string{
   //  const color = this.paginatedColorResult.items.find(c=>c.id === colorId);
   //  return color ? color.colorName: "";
   //}
 
-  handleChange(event: any){
+  handleChange(event: any) {
     const search = event.detail["value"];
     if (search)
-      this.paginatedModelResult.items = this.paginatedModelResult.items.
-        filter(model => model.brandName.toLowerCase().startsWith(search.toLowerCase()))
-    else{
+      this.paginatedModelResult.items = this.paginatedModelResult.items.filter(model => model.brandName.toLowerCase().startsWith(search.toLowerCase()))
+    else {
       this.getModels();
     }
   }
@@ -82,18 +87,18 @@ export class Tab1Page implements OnInit{
   favorite(car: Model) {
     const cars: Model[] | null = localStorage.getItem('favorite-cars') ? JSON.parse(localStorage.getItem("favorite-cars")!) : null
 
-    if (cars){
+    if (cars) {
       const newCars = [...cars, car]
       localStorage.setItem("favorite-cars", JSON.stringify(newCars))
       this.favoritesService.favoriteCars.next(newCars)
-    }else {
+    } else {
       const newCars = [car]
       localStorage.setItem("favorite-cars", JSON.stringify(newCars))
       this.favoritesService.favoriteCars.next(newCars)
     }
   }
 
-  goToBuy(carId: number){
+  goToBuy(carId: number) {
     this.alertController.create({
       header: "Tebrikler",
       message: "Araç kiralama başarılı!",
@@ -101,12 +106,33 @@ export class Tab1Page implements OnInit{
     }).then(controller => controller.present())
   }
 
-  goToCreateModel(){
+  goToCreateModel() {
     this.router.navigateByUrl("createmodel")
   }
 
   goToCarDetails(carId: number) {
     this.router.navigateByUrl(`/car-details/${carId}`);
+  }
+
+  delete(carId: number) {
+    const requestBody = { id: carId };
+
+    const options = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      body: requestBody
+    };
+
+    this.httpClient
+      .request<any>('delete', `${environment.apiUrl}Models`, options)
+      .subscribe(
+        (response) => {
+          console.log('success:',response)
+          window.location.reload();
+        },
+        (error) => {
+          console.log('error:',error)
+        }
+      );
   }
 
 }
